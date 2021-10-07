e134ac59d0f6c124edfe1e10bc54a993f5e88ca4
Nehal <nehal.mohanlal12@gmail.com>
2021-10-07 14:39:21 +0200

    Add project files.

---
+++ yeayeay/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace yeayeay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtStudId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.TxtCourse = new System.Windows.Forms.TextBox();
+            this.btnPopulate = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeaderSid = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeadername = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderlname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderCid = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(557, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Student ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(557, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(557, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Surname ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(557, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Course ID";
+            // 
+            // txtStudId
+            // 
+            this.txtStudId.Location = new System.Drawing.Point(636, 57);
+            this.txtStudId.Name = "txtStudId";
+            this.txtStudId.Size = new System.Drawing.Size(100, 20);
+            this.txtStudId.TabIndex = 5;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(636, 103);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(636, 147);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(100, 20);
+            this.txtSurname.TabIndex = 7;
+            // 
+            // TxtCourse
+            // 
+            this.TxtCourse.Location = new System.Drawing.Point(636, 186);
+            this.TxtCourse.Name = "TxtCourse";
+            this.TxtCourse.Size = new System.Drawing.Size(100, 20);
+            this.TxtCourse.TabIndex = 8;
+            // 
+            // btnPopulate
+            // 
+            this.btnPopulate.Location = new System.Drawing.Point(23, 318);
+            this.btnPopulate.Name = "btnPopulate";
+            this.btnPopulate.Size = new System.Drawing.Size(94, 35);
+            this.btnPopulate.TabIndex = 9;
+            this.btnPopulate.Text = "populate list view";
+            this.btnPopulate.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderSid,
+            this.columnHeadername,
+            this.columnHeaderlname,
+            this.columnHeaderCid});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(23, 26);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(500, 242);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderSid
+            // 
+            this.columnHeaderSid.Text = "Student ID ";
+            this.columnHeaderSid.Width = 84;
+            // 
+            // columnHeadername
+            // 
+            this.columnHeadername.Text = "Name";
+            this.columnHeadername.Width = 102;
+            // 
+            // columnHeaderlname
+            // 
+            this.columnHeaderlname.Text = "Last Name";
+            this.columnHeaderlname.Width = 122;
+            // 
+            // columnHeaderCid
+            // 
+            this.columnHeaderCid.Text = "Course ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnPopulate);
+            this.Controls.Add(this.TxtCourse);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtStudId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtStudId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox TxtCourse;
+        private System.Windows.Forms.Button btnPopulate;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeaderSid;
+        private System.Windows.Forms.ColumnHeader columnHeadername;
+        private System.Windows.Forms.ColumnHeader columnHeaderlname;
+        private System.Windows.Forms.ColumnHeader columnHeaderCid;
+    }
+}
+
